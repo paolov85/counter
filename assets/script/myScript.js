7e26d41e878ee65efb3d03cb47ab9e5a7aaf85f6
paolov85 <92166899+paolov85@users.noreply.github.com>
2023-07-21 09:36:03 +0200

    arrow img fixed

--- assets/script/myScript.js
+++ assets/script/myScript.js
@@ -1,14 +1,16 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-
+    // seleziono gli elementi di cui avrò bisogno
     let toSumm = document.getElementsByClassName('toSumm');
     let display = document.getElementById('display');
     let result = 0;
 
+    //tramite il ciclo for aggiungo un evento click ad ogni pulsante, che lancia la funzione 'somma'
     for (i = 0; i < toSumm.length; i++) {
         toSumm[i].addEventListener('click', somma)
     }
 
+    //la funzione 'somma' ottiene l'id del pulsante cliccato ed in base ad esso assegna un diverso valore da aggiungere/sottrarre al totale, mostrandolo poi sul display
     function somma() {
 
         toAddId = this.id
@@ -42,6 +44,5 @@ window.addEventListener('DOMContentLoaded', function () {
         display.innerHTML = `<p>${result}</p>`;
 
     }
-
 
 });
